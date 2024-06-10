28c1e05ddcf346a51d7f1f2053d5fc9ddedcaec6
Ginger Society Admin <admin@gingersociety.org>
2024-06-10 19:45:02 +0530

    feat: minor cleanup

--- dj/src/models.py
+++ dj/src/models.py
@@ -2,32 +2,46 @@ from ginger.db import models
 
 
 course_type = (
-
     ("compulsary", "Compulsary"),
-
     ("elective", "Elective"),
-
 )
 
 
 class student(models.Model):
     """Lorem ipsum dolem text"""
 
-    name = models.CharField(max_length=150,)
+    name = models.CharField(
+        max_length=150,
+    )
 
-    roll_number = models.CharField(max_length=40,)
+    roll_number = models.CharField(
+        max_length=40,
+    )
 
-    on_scholarship = models.BooleanField(default=True,)
+    on_scholarship = models.BooleanField(
+        default=True,
+    )
 
-    father_name = models.CharField(max_length=100,     null=True,)
+    father_name = models.CharField(
+        max_length=100,
+        null=True,
+    )
 
-    address = models.TextField(max_length=500,)
+    address = models.TextField(
+        max_length=500,
+    )
 
-    data_of_birth = models.DateField(null=True,)
+    data_of_birth = models.DateField(
+        null=True,
+    )
 
-    created_at = models.DateTimeField(auto_now_add=True,)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
 
-    updated_at = models.DateField(auto_now=True,)
+    updated_at = models.DateField(
+        auto_now=True,
+    )
 
     class Meta:
         db_table = "student"
@@ -37,10 +51,16 @@ class enrollment(models.Model):
     """Once again the lorem ipsum text"""
 
     student = models.ForeignKey(
-        'student', related_name='courses', on_delete=models.DO_NOTHING,)
+        "student",
+        related_name="courses",
+        on_delete=models.DO_NOTHING,
+    )
 
     course = models.ForeignKey(
-        'course', on_delete=models.SET_NULL,      null=True,)
+        "course",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
 
     class Meta:
         db_table = "enrollment"
@@ -49,10 +69,15 @@ class enrollment(models.Model):
 class course(models.Model):
     """Another lorem ipsum dolem text"""
 
-    name = models.CharField(max_length=100,)
+    name = models.CharField(
+        max_length=100,
+    )
 
-    type = models.CharField(choices=course_type,
-                            max_length=50,   default='compulsary',)
+    type = models.CharField(
+        choices=course_type,
+        max_length=50,
+        default="compulsary",
+    )
 
     class Meta:
         db_table = "course"
