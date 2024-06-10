a256ec845a5c836198c47de82623f293eb6a9960
Ginger Society Admin <admin@gingersociety.org>
2024-06-10 18:23:15 +0530

    feat: updated forignkey reference to use quoted model name instead of as a variable

--- output/models.py
+++ output/models.py
@@ -1,68 +1,68 @@
 from ginger.db import models
 
 
-
 class student(models.Model):
-    
-    
-     
-        name = models.CharField(  max_length=150,      )
-    
-     
-        roll_number = models.CharField(  max_length=40,      )
-    
-     
-        on_scholarship = models.BooleanField(default=True,       )
-    
-     
-        father_name = models.CharField(  max_length=100,     null=True,  )
-    
-     
-        address = models.TextField(  max_length=500,      )
-    
-        class Meta:
-            db_table = "student"
 
+    name = models.CharField(
+        max_length=150,
+    )
 
+    roll_number = models.CharField(
+        max_length=40,
+    )
+
+    on_scholarship = models.BooleanField(
+        default=True,
+    )
+
+    father_name = models.CharField(
+        max_length=100,
+        null=True,
+    )
+
+    address = models.TextField(
+        max_length=500,
+    )
+
+    class Meta:
+        db_table = "student"
 
 
 course_type = (
-    
-        ("compulsary", "Compulsary"),
-    
-        ("elective", "Elective"),
-    
+    ("compulsary", "Compulsary"),
+    ("elective", "Elective"),
 )
 
 
-
-
 class enrollment(models.Model):
-    
-    
-     
-        student = models.ForeignKey(student,related_name = 'courses', on_delete=models.DO_NOTHING,       )
-    
-     
-        course = models.ForeignKey(course,on_delete=models.SET_NULL,      null=True,  )
-    
-        class Meta:
-            db_table = "enrollment"
 
+    student = models.ForeignKey(
+        "student",
+        related_name="courses",
+        on_delete=models.DO_NOTHING,
+    )
 
+    course = models.ForeignKey(
+        "course",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    class Meta:
+        db_table = "enrollment"
 
 
 class course(models.Model):
-    
-    
-     
-        name = models.CharField(  max_length=100,      )
-    
-     
-        type = models.CharField( choices=course_type, max_length=50,   default='compulsary',   )
-    
-        class Meta:
-            db_table = "course"
 
+    name = models.CharField(
+        max_length=100,
+    )
 
+    type = models.CharField(
+        choices=course_type,
+        max_length=50,
+        default="compulsary",
+    )
 
+    class Meta:
+        db_table = "course"
