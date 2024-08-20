1f31ffa5358e2165d87eaec23cd76a13127198ee
Ginger Society Admin <admin@gingersociety.org>
2024-08-20 20:57:18 +0530

    feat: sepeated runtime sub project into its own repo

--- runner-image/src/apps.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=C0115
-"""App definition for the src app"""
-
-from ginger.apps import AppConfig
-
-
-class SrcConfig(AppConfig):
-    default_auto_field = "ginger.db.models.BigAutoField"
-    name = "src"
